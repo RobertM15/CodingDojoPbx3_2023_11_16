cb9afdbb8f650defc6286d9f0229801b939a9e83
Kasper <rybak.kasper@gmail.com>
2023-11-16 20:16:03 +0100

    something fix

--- test_example.py
+++ test_example.py
@@ -3,4 +3,4 @@ from main import Minefield
 
 def test_known_empty_singular_minefield():
     minefield = Minefield(1,1)
-    assert(minefield.display() == "0"
+    assert(minefield.display() == "0")
