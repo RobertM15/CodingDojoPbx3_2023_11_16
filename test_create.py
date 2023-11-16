efcae586ab1be6a0aa01d32fd562957ee767d087
Kasper <rybak.kasper@gmail.com>
2023-11-16 20:27:19 +0100

    fix tests

--- test_create.py
+++ test_create.py
@@ -2,7 +2,7 @@ from main import Minefield
 
 
 def test_create():
-    minefield = Minefield(3, 4)
+    minefield = Minefield(3, 4, [])
     assert minefield.height == 3
     assert minefield.width == 4
 
--- test_example.py
+++ test_example.py
@@ -2,5 +2,5 @@ from main import Minefield
 
 
 def test_known_empty_singular_minefield():
-    minefield = Minefield(1,1)
+    minefield = Minefield(1,1, [(0, 0)])
     assert(minefield.display() == "0")
