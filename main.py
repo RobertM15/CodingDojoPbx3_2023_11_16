188fa87f0564bc0a14990de2eece290cb3597d0c
Robert Matląg <r.matlag@getprintbox.com>
2023-11-16 19:38:07 +0100

    minefield init

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-class Minefield:
-    def display(self, ):
-        pass
+from itertools import product
 
 
 class Field:
-    pass
+    def __init__(self, x, y, is_mine) -> None:
+        self.x = x
+        self.y = y
+        self.is_mine = is_mine
+
+
+class Minefield:
+    board: list[list[Field]]
+
+    def __init__(self, m, n) -> None:
+        self.m = m
+        self.n = n
+        self.create()
+
+    def create(self):
+        pass
+
+    def get_mine_coordinates(self):
+        pass
+
+    def display(self):
+        pass
+
+    @staticmethod
+    def get_display_value(field: Field):
+        pass
 
